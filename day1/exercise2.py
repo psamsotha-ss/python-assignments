eac8ecbcb92db5106414c86cba269fa3f45ad28e
Paul Samsotha <paul.samsotha@smoothstack.com>
2021-08-05 14:19:19 -0700

    Add day 1 assignments

---
+++ day1/exercise2.py
@@ -0,0 +1,31 @@
+
+def number_1(a, b):
+    print("number 1")
+    print(50 + 50)
+    print(100 - 10)
+
+
+def number_2():
+    print("number 2")
+    print(30 + 6)
+    print(6 ^ 6)
+    print(6 ** 6)
+    print(6 + 6 + 6 + 6 + 6 + 6)
+
+
+def number_3():
+    print("Hello World")
+    print("Hello World : 10")
+
+
+def number_4():
+    p = 800000
+    r = 0.06
+    m = 10000
+
+
+if __name__ == '__main__':
+    number_1()
+    number_2()
+    number_3()
+    number_4()
