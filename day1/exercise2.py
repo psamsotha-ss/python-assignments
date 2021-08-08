b364c6d8ad933659f74808e5769a39896bed36aa
Paul Samsotha <paul.samsotha@smoothstack.com>
2021-08-08 15:42:25 -0700

    Fix day 1 exercises

--- day1/exercise2.py
+++ day1/exercise2.py
@@ -1,5 +1,5 @@
 
-def number_1(a, b):
+def number_1():
     print("number 1")
     print(50 + 50)
     print(100 - 10)
@@ -7,7 +7,7 @@ def number_1(a, b):
 
 def number_2():
     print("number 2")
-    print(30 + 6)
+    print(30 * 6)
     print(6 ^ 6)
     print(6 ** 6)
     print(6 + 6 + 6 + 6 + 6 + 6)
@@ -18,14 +18,15 @@ def number_3():
     print("Hello World : 10")
 
 
-def number_4():
+def number_4(months):
     p = 800000
-    r = 0.06
-    m = 10000
+    r = 0.06 / 12
+    monthly_payment = (p * r * (1 + r) ** months) / ((1 + r) ** months - 1)
+    print(monthly_payment)
 
 
 if __name__ == '__main__':
-    number_1()
-    number_2()
-    number_3()
-    number_4()
+    # number_1()
+    # number_2()
+    # number_3()
+    number_4(480)
